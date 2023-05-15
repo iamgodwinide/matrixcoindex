9f096aa4b61ca217e3d96346d95884ab49cde9ef
Godwin Idemudia <godwinide@gmail.com>
2023-05-15 18:42:27 +0100

    new

---
+++ assets/js/app.js
@@ -0,0 +1,4 @@
+const handleTweet = () => {
+    const tweet = `I have escaped the $Matrix @matrixethcoin %0A%0A`;
+    window.open(`https://twitter.com/intent/tweet?text=${tweet}&url=https://matrixcoindex.xyz`, '_blank');
+}
